ae10fd293fa3eacf5673f5dd808865cfb7343835
surverules <surverules@gmail.com>
2018-02-02 06:55:49 -0600

    Added Chapter Package and created 1st Class in the package.

---
+++ src/main/java/chapter01/Animal.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+public class Animal {
+
+    String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+}

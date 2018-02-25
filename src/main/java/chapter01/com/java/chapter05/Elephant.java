ac70f8fce83ee38aa1438357d85faf755813b9e8
surverules <surverules@gmail.com>
2018-02-25 14:32:27 -0600

    Chapter 5 classes addition

---
+++ src/main/java/chapter01/com/java/chapter05/Elephant.java
@@ -0,0 +1,5 @@
+package chapter01.com.java.chapter05;
+
+public class Elephant extends Mammal {
+
+}
---
+++ src/main/java/chapter01/com/java/chapter05/Mammal.java
@@ -0,0 +1,8 @@
+package chapter01.com.java.chapter05;
+
+public class Mammal {
+
+    private String name;
+
+    public Mammal(){}
+}
